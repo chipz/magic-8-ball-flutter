8a163a925f93f87d0126b1822ccb78df823a3330
fajrhf <fajr.hf@gmail.com>
2020-02-02 10:42:32 +0700

    Added random logic to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue,
+        ),
+          body: BodyState(),
+        ),
       ),
     );
+
+
+class _BodyPageState extends State<BodyState> {
+
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                  print("Button pressed! number is $ballNumber");
+                });
+              },
+              child: Image.asset("images/ball$ballNumber.png"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class BodyState extends StatefulWidget {
+  @override
+  createState() => _BodyPageState();
+}
